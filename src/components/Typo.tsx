7ea0181cb7c3d0640e507356485d0a53b9afd88e
Andres <esteban.altamura@gmail.com>
2024-09-06 19:37:36 +0200

    update

--- src/components/Typo.tsx
+++ src/components/Typo.tsx
@@ -3,51 +3,91 @@ import React from 'react';
 type TypoProps = {
   type:
     | 'title1'
+    | 'title1Semibold'
     | 'title2'
+    | 'title2Semibold'
     | 'title3'
+    | 'title3Semibold'
     | 'title4'
+    | 'title4Semibold'
+    | 'title5'
+    | 'title5Semibold'
+    | 'title6'
+    | 'title6Semibold'
     | 'body1'
+    | 'body1Semibold'
     | 'body2'
+    | 'body2Semibold'
     | 'body3'
+    | 'body3Semibold'
     | 'body4'
+    | 'body4Semibold'
     | 'caption'
     | 'overline';
   children: React.ReactNode;
 };
 
 const classMap: { [key in TypoProps['type']]: string } = {
-  title1: 'text-title1',
-  title2: 'text-title2',
-  title3: 'text-title3',
-  title4: 'text-title4',
-  body1: 'text-body1',
-  body2: 'text-body2',
-  body3: 'text-body3',
-  body4: 'text-body4',
-  caption: 'text-caption',
-  overline: 'text-overline',
+  title1: 'text-title1 font-normal',
+  title1Semibold: 'text-title1 font-semibold',
+  title2: 'text-title2 font-normal',
+  title2Semibold: 'text-title2 font-semibold',
+  title3: 'text-title3 font-normal',
+  title3Semibold: 'text-title3 font-semibold',
+  title4: 'text-title4 font-normal',
+  title4Semibold: 'text-title4 font-semibold',
+  title5: 'text-title5 font-normal',
+  title5Semibold: 'text-title5 font-semibold',
+  title6: 'text-title6 font-normal',
+  title6Semibold: 'text-title6 font-semibold',
+
+  body1: 'text-body1 font-normal',
+  body1Semibold: 'text-body1 font-semibold',
+  body2: 'text-body2 font-normal',
+  body2Semibold: 'text-body2 font-semibold',
+  body3: 'text-body3 font-normal',
+  body3Semibold: 'text-body3 font-semibold',
+  body4: 'text-body4 font-normal',
+  body4Semibold: 'text-body4 font-semibold',
+
+  caption: 'text-caption font-normal',
+  overline: 'text-overline font-normal',
 };
 
 const Typo = ({ type, children }: TypoProps) => {
   const Tag = (() => {
     switch (type) {
       case 'title1':
+      case 'title1Semibold':
         return 'h1';
       case 'title2':
+      case 'title2Semibold':
         return 'h2';
       case 'title3':
+      case 'title3Semibold':
         return 'h3';
       case 'title4':
+      case 'title4Semibold':
         return 'h4';
+      case 'title5':
+      case 'title5Semibold':
+        return 'h5';
+      case 'title6':
+      case 'title6Semibold':
+        return 'h6';
       case 'body1':
+      case 'body1Semibold':
       case 'body2':
+      case 'body2Semibold':
       case 'body3':
+      case 'body3Semibold':
       case 'body4':
+      case 'body4Semibold':
       case 'caption':
       case 'overline':
         return 'p';
       default:
-        return 'p'; // fallback
+        return 'p'; // Fallback en caso de tipo no válido
     }
   })();
 
